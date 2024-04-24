6be37c0cad4097cea8e6635caac5151ac05c742c
EngSamiran <samiranwwcst@gmail.com>
2024-04-24 15:27:07 +0530

    Update AllRequestPath file

--- src/main/java/com/eas/utils/AllRequestPath.java
+++ src/main/java/com/eas/utils/AllRequestPath.java
@@ -5,6 +5,6 @@ public final class AllRequestPath {
 	public static final String ADD_EMPLOYEE = "/admin/add-employee";
 	public static final String UPDATE_EMPLOYEE_BY_ID = "/admin/update-employee/{empId}";
 	public static final String DELETE_EMPLOYEE_BY_ID = "/admin/delete-employee/{empId}";
-	public static final String GET_EMPLOYEE_BY_ID = "/employees/{empId}";
+	public static final String GET_EMPLOYEE_BY_ID = "/employee/{empId}";
 	public static final String GET_ALL_EMPLOYEE = "/employees";
 }
